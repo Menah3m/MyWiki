98b22ba163dac64956944a4130b4bbe94f86eeda
yasu <568200065@qq.com>
2020-03-24 22:35:20 +0900

    修改templates文件

--- wiki/urls.py
+++ wiki/urls.py
@@ -6,4 +6,5 @@ app_name = 'wiki'
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('<int:id>/',views.item_detail, name='item_detail'),
 ]
--- wiki/views.py
+++ wiki/views.py
@@ -9,3 +9,9 @@ def index(request):
     categories = Category.objects.all()
     context = {'categories': categories}
     return render(request, 'wiki/index.html', context)
+
+def item_detail(request, id):
+    items = Item.objects.filter(category_id=id)
+    # text = items.category_set.all
+    context = {'items':items}
+    return render(request, 'wiki/item_detail.html', context)
