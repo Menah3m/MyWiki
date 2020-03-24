1eb1ec30ebccdeec8089bf18a755e5f47e3393d2
yasu <568200065@qq.com>
2020-03-24 23:53:06 +0900

    修改templates文件

--- wiki/urls.py
+++ wiki/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'wiki'
 
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('<int:id>/',views.item_detail, name='item_detail'),
+    path('', views.index, name='index'),
+    path('category/<int:id>', views.item_list, name='item_list'),
+    path('item/<int:id>', views.item_detail, name='item_detail')
 ]
--- wiki/views.py
+++ wiki/views.py
@@ -10,8 +10,14 @@ def index(request):
     context = {'categories': categories}
     return render(request, 'wiki/index.html', context)
 
-def item_detail(request, id):
+def item_list(request, id):
     items = Item.objects.filter(category_id=id)
     # text = items.category_set.all
-    context = {'items':items}
+    context = {'items': items}
+    return render(request, 'wiki/item_list.html', context)
+
+def item_detail(request, id):
+    items = Item.objects.get(id=id)
+    context = {'item_name':items.item_name, 'item_text':items.item_body}
+
     return render(request, 'wiki/item_detail.html', context)
