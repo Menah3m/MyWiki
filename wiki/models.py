cea9e8cbbaace6f90a67a6e19ff52b7543a0c161
yasu <568200065@qq.com>
2020-03-24 21:19:39 +0900

    添加index.html和base.html

--- wiki/models.py
+++ wiki/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 
 class Category(models.Model):
+    '''知识点领域分类'''
     category_name = models.CharField(max_length=50)
-    category_created = models.DateTimeField()
+    category_created = models.DateTimeField(auto_now_add=True)
     category_creator = models.CharField(max_length=50)
 
     def __str__(self):
         return self.category_name
 
 class Item(models.Model):
+    '''具体的知识点'''
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default='')
     item_name = models.CharField(max_length=50)
     item_body = models.TextField()
-    item_created = models.DateTimeField()
+    item_created = models.DateTimeField(auto_now_add=True)
     item_creator = models.CharField(max_length=50)
 
     def __str__(self):
--- wiki/views.py
+++ wiki/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Category,Item
+
 
 def index(request):
-    return HttpResponse("Hello.This is MyWiki index.")
+    '''返回主页内容'''
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'wiki/index.html', context)
